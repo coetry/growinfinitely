92d215d7a76697705b7bcfaacf4397b159b30c28
Allen Hai <hai@growinfinitely.com>
2017-09-09 10:22:17 -0500

    added homepage links

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
-export default () => (
-  <h1>yo</h1>
+import Link from 'next/link'
+
+const Home = () => (
+  <div> 
+	  <Link href="/values">VALUES</Link><br/>
+	  <Link href="/services">SERVICES</Link><br/>
+	  <Link href="/work-with-us">WORK W/ US</Link><br/>
+	  <Link href="/hire">HIRE US</Link><br/>
+	</div>
 )
+
+export default Home
