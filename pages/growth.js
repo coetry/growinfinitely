657e41b60827410ee6588b864a8d94c6f2e4a48a
Zubair Mughal <zubi@Zubairs-MacBook-Pro.local>
2017-12-05 16:54:00 -0600

    updated growth page with more content

--- pages/growth.js
+++ pages/growth.js
@@ -16,27 +16,30 @@ export default () => (
         <p>
           Why do you exist? A strong brand identity allows you to answer this question. It appeals to the senses. It creates
           curiosity, excitement, and genuine emotions in followers of your brand.
-          Its a system that creates consistency across all your touchpoints. We can help you get started.
+          It is a system that creates consistency across all your touchpoints.
         </p>
       </div>
 
       <div className="growth-services">
         <h2>Discoverability</h2>
         <p>
-       We help you get found. We put your brand in the right places, so people can discover and start a relationship with you.
+       We help you get found. We put your brand in the right places, so your people can discover and start a relationship with you.
         </p>
       </div>
 
       <div className="growth-services">
         <h2>Storytelling</h2>
         <p>
-          We produce engaging media to tell stories about your brand.
+          Compelling visual and auditorial pieces serve as a way to illustrate the essence of your brand. We create these illustrations with the mindset of producing a reaction within your audience. This requires us to think rationally and creativly to design a story using conventional rehtoric.
         </p>
+        <p> Uniting an idea with an emotion is a powerful way to produce a reaction from your audience. Compelling visual and auditorial pieces serve as a way to illustrate the essence of your brand. We enjoy sitting down and thinking creativly to design a story using unconventional rehtoric.</p>
       </div>
       <div className="growth-services">
         <h2>Social</h2>
         <p>
-          We help you communicate your brand identity with your audience.
+        We use your brand’s voice to communicate with people. We believe in being apart of your follower's life by enriching their experience throughout different social channels. It not about selling your service or product, But instead effectively selling your existence and vision.
+        <p> social media has no secret, you just have to treat your followers as humans. </p>
+
         </p>
       </div>
       </div>
